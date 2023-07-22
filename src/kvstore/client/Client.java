0229330d2dc850e1a068d75be4f43d3d06d7e9f1
Lucas Henrique Gois de Campos <32200215+lhgcampos97@users.noreply.github.com>
2023-07-21 23:33:29 -0300

    Add files via upload

--- src/kvstore/client/Client.java
+++ src/kvstore/client/Client.java
@@ -6,83 +6,87 @@ import kvstore.message.Message;
 
 import java.io.*;
 import java.net.*;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Client {
-	
+
 	private static final Gson gson = new Gson();
-	
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the number of servers: ");
-        int numServers = scanner.nextInt();
-        scanner.nextLine(); // Consumes the newline character
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        String[] serverAddresses = new String[numServers];
+		System.out.print("Enter the number of servers: ");
+		int numServers = scanner.nextInt();
+		scanner.nextLine(); // Consumes the newline character
 
-        for (int i = 0; i < numServers; i++) {
-            System.out.print("Enter the IP address and port number of server " + (i + 1) + " (e.g., localhost:8000): ");
-            serverAddresses[i] = scanner.nextLine();
-        }
+		String[] serverAddresses = new String[numServers];
 
-        boolean exit = false;
+		for (int i = 0; i < numServers; i++) {
+			System.out.print("Enter the IP address and port number of server " + (i + 1) + " (e.g., localhost:8000): ");
+			serverAddresses[i] = scanner.nextLine();
+		}
 
-        do {
-            System.out.print("Enter the key (or type 'exit' to quit): ");
-            String key = scanner.nextLine();
+		boolean exit = false;
 
-            if (key.equalsIgnoreCase("exit")) {
-                exit = true;
-            } else {
-                System.out.print("Enter the command (PUT or GET): ");
-                String command = scanner.nextLine();
+		do {
+			System.out.print("Enter the key (or type 'exit' to quit): ");
+			String key = scanner.nextLine();
 
-                if (command.equalsIgnoreCase("PUT")) {
-                    System.out.print("Enter the value: ");
-                    String value = scanner.nextLine();
+			if (key.equalsIgnoreCase("exit")) {
+				exit = true;
+			} else {
+				System.out.print("Enter the command (PUT or GET): ");
+				String command = scanner.nextLine();
 
-                    Message message = new Message(command, key, value);
-                    String response = sendRequest(serverAddresses, message);
-                    System.out.println("Response: " + response);
-                } else if (command.equalsIgnoreCase("GET")) {
-                    Message message = new Message(command, key, null);
-                    String response = sendRequest(serverAddresses, message);
-                    System.out.println("Response: " + response);
-                } else {
-                    System.out.println("Invalid command");
-                }
-            }
-        } while (!exit);
-    }
+				if (command.equalsIgnoreCase("PUT")) {
+					System.out.print("Enter the value: ");
+					String value = scanner.nextLine();
 
-    private static String sendRequest(String[] serverAddresses, Message message) {
-        String response = "Error: No servers available";
+					Message message = new Message(command, key, value);
+					String response = sendRequest(serverAddresses, message);
+					System.out.println("Response: " + response);
+				} else if (command.equalsIgnoreCase("GET")) {
+					Message message = new Message(command, key, null);
+					String response = sendRequest(serverAddresses, message);
+					System.out.println("Response: " + response);
+				} else {
+					System.out.println("Invalid command");
+				}
+			}
+		} while (!exit);
+	}
 
-        for (String serverAddress : serverAddresses) {
-            String[] parts = serverAddress.split(":");
-            String ipAddress = parts[0];
-            int port = Integer.parseInt(parts[1]);
+	private static String sendRequest(String[] serverAddresses, Message message) {
+		Random random = new Random();
+		String randomServerAddress = serverAddresses[random.nextInt(serverAddresses.length)];
 
-            try (
-                Socket socket = new Socket(ipAddress, port);
-                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))
-            ) {
-                String jsonMessage = gson.toJson(message);
-                writer.println(jsonMessage);
+		String response = "Error: No servers available";
 
-                response = reader.readLine();
-                if (!response.equals("Error: No servers available")) {
-                    break;
-                }
-            } catch (IOException e) {
-                // Server is not available
-            }
-        }
+		String[] parts = randomServerAddress.split(":");
+		String ipAddress = parts[0];
+		int port = Integer.parseInt(parts[1]);
 
-        return response;
-    }
+
+		try (
+				Socket socket = new Socket(ipAddress, port);
+				PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+				) {
+			String jsonMessage = gson.toJson(message);
+			writer.println(jsonMessage);
+
+			response = reader.readLine();
+			if (!response.equals("Error: No servers available")) {
+				return response;
+			}
+		} catch (IOException e) {
+			// Server is not available
+		}
+
+
+		return response;
+	}
 
 
 }
--- src/kvstore/server/Server.java
+++ src/kvstore/server/Server.java
@@ -10,20 +10,39 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Server {
-	private ServerSocket serverSocket;
+    private ServerSocket serverSocket;
     private Map<String, String> store;
+    private Map<String, String> serverAddresses;
     private boolean isLeader;
     private static final Gson gson = new Gson();
 
     public Server(boolean isLeader) {
-    	this.isLeader = isLeader;
+        this.isLeader = isLeader;
         store = new HashMap<>();
+        serverAddresses = new HashMap<>();
     }
 
     public void start(String ipAddress, int port, String leaderIp, int leaderPort) {
         try {
             serverSocket = new ServerSocket(port, 10, InetAddress.getByName(ipAddress));
             System.out.println("Server started on " + ipAddress + ":" + port);
+
+            if (isLeader) {
+                // Add leader's address to the table
+                serverAddresses.put(ipAddress + ":" + port, "Leader");
+
+                // Read secondary server IP and port from the keyboard
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the number of secondary servers: ");
+                int numSecondaryServers = scanner.nextInt();
+                scanner.nextLine(); // Consumes the newline character
+
+                for (int i = 0; i < numSecondaryServers; i++) {
+                    System.out.print("Enter the IP address and port number of secondary server " + (i + 1) + " (e.g., localhost:8001): ");
+                    String secondaryServerAddress = scanner.nextLine();
+                    serverAddresses.put(secondaryServerAddress, "Secondary");
+                }
+            }
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
@@ -39,28 +58,27 @@ public class Server {
     }
 
     public static void main(String[] args) {
-
         // Read IP and port from the keyboard
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.print("Enter the IP address: ");
         String ipAddress = scanner.nextLine();
         System.out.print("Enter the port number: ");
         int port = scanner.nextInt();
         scanner.nextLine(); // Consumes the newline character
-        
+
         System.out.print("Enter the IP address of the leader: ");
         String leaderIp = scanner.nextLine();
         System.out.print("Enter the port number of the leader: ");
         int leaderPort = scanner.nextInt();
-        
+
         boolean isLeader = false;
-        
+
         if (ipAddress.equals(leaderIp) && port == leaderPort) {
-        	isLeader = true;
-        	System.out.print("This server is the leader! \n");
+            isLeader = true;
+            System.out.print("This server is the leader! \n");
         }
-        
+
         Server server = new Server(isLeader);
         server.start(ipAddress, port, leaderIp, leaderPort);
     }
@@ -78,27 +96,31 @@ public class Server {
 
         public void run() {
             try (
-                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true)
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true)
             ) {
-            	String jsonRequest = reader.readLine();
+                String jsonRequest = reader.readLine();
                 Message request = gson.fromJson(jsonRequest, Message.class);
                 System.out.println("Received request: " + jsonRequest);
 
                 String response;
-                if (isLeader) {
-                    // O servidor atual é o líder, manipule a requisição localmente
-                    if ("GET".equals(request.getCommand())) {
-                        response = handleGet(request.getKey());
-                    } else if ("PUT".equals(request.getCommand())) {
-                        response = handlePut(request.getKey(), request.getValue());
+
+                if ("GET".equals(request.getCommand())) {
+                    response = handleGet(request.getKey());
+                } else if ("PUT".equals(request.getCommand())) {
+                    if (isLeader) {
+                        sendReplication(request);
+                        handlePut(request.getKey(), request.getValue());
+                        response = "PUT OK";
                     } else {
-                        response = "Invalid command";
+                        // Encaminhe a requisição para o líder
+                        String jsonResponse = forwardRequestToLeader(jsonRequest);
+                        response = gson.fromJson(jsonResponse, String.class);
                     }
+                } else if ("REPLICATION".equals(request.getCommand())) {
+                    response = handleReplication(request.getKey(), request.getValue());
                 } else {
-                    // Encaminhe a requisição para o líder
-                    String jsonResponse = forwardRequestToLeader(jsonRequest);
-                    response = gson.fromJson(jsonResponse, String.class);
+                    response = "Invalid command";
                 }
 
                 writer.println(gson.toJson(response));
@@ -113,12 +135,37 @@ public class Server {
                 }
             }
         }
+        
+
+        private void sendReplication(Message request) {
+            for (String serverAddress : serverAddresses.keySet()) {
+                if (serverAddress.equals(leaderIp + ":" + leaderPort)) {
+                    continue; // Skip the leader itself
+                }
+
+                String[] parts = serverAddress.split(":");
+                String ipAddress = parts[0];
+                int port = Integer.parseInt(parts[1]);
+
+                try (
+                        Socket socket = new Socket(ipAddress, port);
+                        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)
+                ) {
+                    // Create a replication message
+                    Message replicationMessage = new Message("REPLICATION", request.getKey(), request.getValue());
+                    String jsonMessage = gson.toJson(replicationMessage);
+                    writer.println(jsonMessage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         private String forwardRequestToLeader(String jsonRequest) {
             try (
-                Socket leaderSocket = new Socket(leaderIp, leaderPort);
-                PrintWriter writer = new PrintWriter(leaderSocket.getOutputStream(), true);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(leaderSocket.getInputStream()))
+                    Socket leaderSocket = new Socket(leaderIp, leaderPort);
+                    PrintWriter writer = new PrintWriter(leaderSocket.getOutputStream(), true);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(leaderSocket.getInputStream()))
             ) {
                 writer.println(jsonRequest);
                 String jsonResponse = reader.readLine();
@@ -132,15 +179,21 @@ public class Server {
         private String handleGet(String key) {
             String value = store.get(key);
             if (value != null) {
-                return "OK " + value;
+                return value;
             } else {
                 return "Key not found";
             }
         }
 
-        private String handlePut(String key, String value) {
+        private void handlePut(String key, String value) {
             store.put(key, value);
-            return "OK";
+            //return "PUT OK";
+        }
+
+        private String handleReplication(String key, String value) {
+            // Handle the replication message received from the leader
+            store.put(key, value);
+            return "REPLICATION_OK"; // For secondary servers, just acknowledge the replication message
         }
     }
 }
