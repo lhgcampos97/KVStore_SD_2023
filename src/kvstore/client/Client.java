c332e4da4ece9af7146484aebb3836b714d42f02
Lucas Henrique Gois de Campos <32200215+lhgcampos97@users.noreply.github.com>
2023-07-24 12:14:14 -0300

    Comando INIT

--- src/kvstore/client/Client.java
+++ src/kvstore/client/Client.java
@@ -1,6 +1,5 @@
 package kvstore.client;
 
-
 import com.google.gson.Gson;
 import kvstore.message.Message;
 
@@ -21,17 +20,32 @@ import java.util.Scanner;
 public class Client {
 
 	private static final Gson gson = new Gson();
-    private static Map<String, Long> timestampStore = new HashMap<>(); // Mapa para armazenar pares chave-timestamp
+	private static Map<String, Long> timestampStore = new HashMap<>(); // Mapa para armazenar pares chave-timestamp
 
-    /**
-     * Método principal para executar o cliente.
-     * @param args Argumentos da linha de comando.
-     */
+	/**
+	 * Método principal para executar o cliente.
+	 * @param args Argumentos da linha de comando.
+	 */
 	public static void main(String[] args) {
-		
 		Scanner scanner = new Scanner(System.in);
 
-		// Lê o número de servidores e os endereços IP e portas de cada servidor a partir da entrada do usuário.
+		System.out.print("Enter the command (INIT): ");
+		String initCommand = scanner.nextLine();
+
+		if (initCommand.equalsIgnoreCase("INIT")) {
+			// Inicializa os servidores a partir da entrada do usuário.
+			String[] serverAddresses = initializeServers(scanner);
+			startClient(scanner,serverAddresses);
+		} else {
+			System.out.println("Invalid command. Client initialization failed.");
+		}
+	}
+
+	/**
+	 * Inicializa os servidores a partir da entrada do usuário.
+	 * @param scanner O objeto Scanner para ler a entrada do usuário.
+	 */
+	private static String[] initializeServers(Scanner scanner) {
 		System.out.print("Enter the number of servers: ");
 		int numServers = scanner.nextInt();
 		scanner.nextLine(); 
@@ -42,66 +56,76 @@ public class Client {
 			System.out.print("Enter the IP address and port number of server " + (i + 1) + " (e.g., localhost:8000): ");
 			serverAddresses[i] = scanner.nextLine();
 		}
+		
+		return serverAddresses;
+	}
 
+	/**
+	 * Inicia o loop do cliente para solicitar comandos e interagir com os servidores.
+	 * @param scanner O objeto Scanner para ler a entrada do usuário.
+	 */
+	private static void startClient(Scanner scanner, String[] serverAddresses) {
 		boolean exit = false;
 
-		// Inicia um loop para solicitar comandos do usuário até que ele insira "exit".
 		do {
-			System.out.print("Enter the key (or type 'exit' to quit): ");
-			String key = scanner.nextLine();
 
-			if (key.equalsIgnoreCase("exit")) {
+			// Trata as solicitações do usuário (PUT ou GET) e exibe as respostas recebidas dos servidores.
+			System.out.print("Enter the command (PUT or GET): ");
+			String command = scanner.nextLine();
+
+			if (command.equalsIgnoreCase("exit")) {
 				exit = true;
+			} else if (command.equalsIgnoreCase("PUT")) {
+
+				System.out.print("Enter the key (or type 'exit' to quit): ");
+				String key = scanner.nextLine();
+
+				System.out.print("Enter the value: ");
+				String value = scanner.nextLine();
+
+				long timestamp = System.currentTimeMillis(); // Armazena o timestamp atual
+
+				Message message = new Message(command, key, value, timestamp);
+				String response = sendRequest(serverAddresses, message);
+				System.out.println("Response: " + response);
+
+				Message responseJson = gson.fromJson(response, Message.class);
+				timestampStore.put(responseJson.getKey(), responseJson.getTimestamp());
+
+			} else if (command.equalsIgnoreCase("GET")) {
+
+				System.out.print("Enter the key (or type 'exit' to quit): ");
+				String key = scanner.nextLine();
+
+				// Caso a chave não tenha um timestamp atrelado, retorna 0
+				Long timestamp = timestampStore.getOrDefault(key, 0L);
+
+				Message message = new Message(command, key, null, timestamp); 
+				String response = sendRequest(serverAddresses, message);
+				System.out.println("Response: " + response);
+
+				Message responseJson = gson.fromJson(response, Message.class);
+				timestampStore.put(responseJson.getKey(), responseJson.getTimestamp());
+
 			} else {
-				
-		        // Trata as solicitações do usuário (PUT ou GET) e exibe as respostas recebidas dos servidores.
-				System.out.print("Enter the command (PUT or GET): ");
-				String command = scanner.nextLine();
-
-				if (command.equalsIgnoreCase("PUT")) {
-					System.out.print("Enter the value: ");
-					String value = scanner.nextLine();
-
-					long timestamp = System.currentTimeMillis(); // Armazena o timestamp atual
-					
-                    Message message = new Message(command, key, value, timestamp);
-                    String response = sendRequest(serverAddresses, message);
-                    System.out.println("Response: " + response);
-                    
-                    Message responseJson = gson.fromJson(response, Message.class);
-                    timestampStore.put(responseJson.getKey(), responseJson.getTimestamp());
-                    
-				} else if (command.equalsIgnoreCase("GET")) {
-					
-					// Caso a chave não tenha um timestamp atrelado, retorna 0
-					Long timestamp = timestampStore.getOrDefault(key, 0L);
-					
-					Message message = new Message(command, key, null, timestamp); 
-                    String response = sendRequest(serverAddresses, message);
-                    System.out.println("Response: " + response);
-
-                    Message responseJson = gson.fromJson(response, Message.class);
-                    timestampStore.put(responseJson.getKey(), responseJson.getTimestamp());
-                    
-				} else {
-					System.out.println("Invalid command");
-				}
+				System.out.println("Invalid command");
 			}
+
 		} while (!exit);
 	}
 
 	/**
-     * Método para enviar uma solicitação ao servidor selecionado aleatoriamente.
-     * @param serverAddresses Os endereços IP e portas dos servidores disponíveis.
-     * @param message A mensagem de solicitação a ser enviada ao servidor.
-     * @return A resposta recebida do servidor ou uma mensagem de erro, caso não seja possível conectar ao servidor.
-     */
+	 * Método para enviar uma solicitação ao servidor selecionado aleatoriamente.
+	 * @param serverAddresses Os endereços IP e portas dos servidores disponíveis.
+	 * @param message A mensagem de solicitação a ser enviada ao servidor.
+	 * @return A resposta recebida do servidor ou uma mensagem de erro, caso não seja possível conectar ao servidor.
+	 */
 	private static String sendRequest(String[] serverAddresses, Message message) {
 		// Seleciona aleatoriamente um servidor para enviar a solicitação.
 		Random random = new Random();
 		String randomServerAddress = serverAddresses[random.nextInt(serverAddresses.length)];
 
-		String response = "Error: No servers available";
+		String response = gson.toJson(new Message("Error: No servers available", "", "", 0L));
 
 		String[] parts = randomServerAddress.split(":");
 		String ipAddress = parts[0];
@@ -116,18 +140,15 @@ public class Client {
 			String jsonMessage = gson.toJson(message);
 			writer.println(jsonMessage);
 
-			// Retorna a resposta recebida do servidor ou uma mensagem de erro, caso não seja possível conectar ao servidor.
+			// Retorna a resposta recebida do servidor.
 			response = reader.readLine();
-			if (!response.equals("Error: No servers available")) {
-				return response;
-			}
+			return response;
 		} catch (IOException e) {
 			// Servidor não disponível
 		}
 
-
+		// Ou uma mensagem de erro, caso não seja possível conectar ao servidor.
 		return response;
 	}
-
 
 }
